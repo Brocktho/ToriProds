f012e52e24b19672b07f4d94640cf40f100e5d58
Brocktho <bcd5235@psu.edu>
2022-03-08 21:27:50 -0500

    all but the videos

--- src/components/galleryMount.jsx
+++ src/components/galleryMount.jsx
@@ -12,7 +12,7 @@ const GalleryMount = ({toggle, set, target}) => {
         reverse: toggle,
     });
 
-    return GalleryStyle((styles, item) => item && <animated.div style={{...styles}} className="fixed w-screen h-screen flex flex-row items-center justify-center" onClick={() => set(false)}>
+    return GalleryStyle((styles, item) => item && <animated.div style={{...styles}} className="fixed w-screen h-screen flex flex-row items-center justify-center z-20" onClick={() => set(false)}>
             <svg className="w-12 h-12 absolute cursor-pointer top-16 right-4" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
             <div className="w-3/4 h-3/4 flex flex-col items-center justify-center" onClick={(e) => e.stopPropagation()}>
                 <VideoPlayer className="w-full h-4/5" controls={true} src={target.video} poster={target.image} width="720" height="420" onClick={(e) => e.stopPropagation()}/>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./src/components/*", "./src/pages/Home.jsx"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Tangerine', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
